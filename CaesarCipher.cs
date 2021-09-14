202ed54865106d988628b6b343a53d93bd67a5d9
baumertjonathan <youturn98@outlook.com>
2021-09-14 10:06:16 -0600

    Add project files.

---
+++ CaesarCipher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class CaesarCipher
+{
+    public CaesarCipher() 
+    {
+        string Alphabet = "abcdefghijklmnopqrstuvwxyz";
+    }
+
+    public string Encrypt(string text, int key) 
+    {
+        string Result = "";
+
+        for (int i = 0; i <= text.Length; i++) 
+        {
+            
+        }
+
+        return Result;
+    }
+
+    public string Decrypt(string text, int key) 
+    {
+        string Result = "";
+
+        return Result;
+    }
+}
